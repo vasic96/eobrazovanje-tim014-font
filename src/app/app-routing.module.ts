471b11ee96703c867229b6adb2edd5436d5734b5
Aleksandar Vasic <coavasic96@gmail.com>
2019-01-07 01:22:43 +0100

    Lista dokumenta, dodvanje, brisanje

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DokumentListComponent } from './dokument-list/dokument-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dokumenti',component:DokumentListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { MaterialModule } from './material.module';
+import { DokumentListComponent } from './dokument-list/dokument-list.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { DokumentService } from './services/dokument.service';
+import { FormsModule } from '@angular/forms';
+import { NoviDokumentDialog } from './dialogs/novi.dokument.dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DokumentListComponent,
+    NoviDokumentDialog
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    FormsModule
+
   ],
-  providers: [],
+  entryComponents:[NoviDokumentDialog],
+  providers: [DokumentService,HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
